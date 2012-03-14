98d386289d17bc857abb9abac6180a991640c7c3
airportyh <airportyh@gmail.com>
2012-03-14 17:33:40 -0400

    Before trying globals.

--- song.js
+++ song.js
@@ -1,17 +1,17 @@
-function play(){
-    
+!function(){
     var dol = 523.25
       , re = 587.33
       , mi = 659.26
       , so = 394.995
       , fa = 349.23
+      , mib = 311.13
       , la_ = 220
       , ti = 246.94
       , mi_ = mi / 2
       , dol_ = dol / 2
       , la = la_ * 2
       
-    var notes = [
+    var sectionA = [
         [11, dol],
         [1, re],
         [12, dol],
@@ -28,18 +28,44 @@ function play(){
         [9, dol],
         [2, mi_],
         [1, fa],
-        [23, so],
-        [1, so]
+        [24, so]
     ]
+    
+    var sectionB = [
+        [2, fa],
+        [2, so],
+        [1, la],
+        [1, dol],
+        [4, ti],
+        [1, dol],
+        [1, ti],
+        [4, la],
+        [1, fa],
+        [1, la],
+        [6, so],
+        [2, fa],
+        [2, so],
+        [1, la],
+        [1, dol],
+        [4, re],
+        [1, re],
+        [1, dol],
+        [1, mi],
+        [11, so*2]
+    ]
+    
+    var notes = [].concat(sectionA, sectionB, sectionB, sectionA)
     
     var I = [dol_, mi_, so, mi_]
       , VIII = [la_, dol_, mi_, la]
       , IV = [fa/2, la_, dol_, fa]
       , V = [so/2, ti, re/2, ti]
-    var chords = [I, VIII, IV, V, I, VIII, V, V]
-    var comp = []
-    chords.forEach(function(chord){
-        [0,1,2,3,2,1,0,1,2,3,2,1].forEach(function(i){
+    
+    var compA = [I, I, VIII, VIII, IV, IV, V, V, I, I, VIII, VIII, V, V, V, V]
+      , compB = [IV, V, IV, V, IV, V, I, I]
+      , comp = []
+    ![].concat(compA, compB, compB, compA).forEach(function(chord){
+        [0,1,2,3,2,1].forEach(function(i){
             comp.push([1, chord[i]])
         })
     })
@@ -50,45 +76,40 @@ function play(){
       , sampleRate = 44100
       , trackCursors = [0, 0]
       , currNoteDur = []
-      , bufferSize = sampleRate / 2
       , writePos = 0
-      , done = false
+      , taper = 5000
       
     audio.mozSetup(1, sampleRate)
 
     function writeBits(){
-        var toWrite = audio.mozCurrentSampleOffset() + bufferSize - writePos
-        if (toWrite > 0){
+        var toWrite = audio.mozCurrentSampleOffset() + sampleRate / 2 - writePos
+        if (toWrite){
             var data = new Float32Array(toWrite)
             for (var i = 0; i < toWrite; i++, writePos++){
-                var value = 0
                 for (var j = 0; j < 2; j++){
                     var cursor = trackCursors[j]
-                    var currNote = tracks[j][cursor]
+                      , currNote = tracks[j][cursor]
                     if (!currNote){
                         trackCursors[j] = 0
                         continue
                     }
-                    var freq = currNote[1]
                     var noteDur = currNoteDur[j]
-                    if (noteDur == null){
+                    if (!noteDur){
                         noteDur = currNoteDur[j] = [0, currNote[0] * sampleRate / 3]
-                    }else if (noteDur[1] === 0){
+                    }else if (!noteDur[1]){
                         trackCursors[j]++
                         currNoteDur[j] = null
                         break
                     }else{
-                        currNoteDur[j][1]--
-                        currNoteDur[j][0]++
+                        noteDur[1]--
+                        noteDur[0]++
                     }
-                    value += Math.sin(writePos * Math.PI * 2 * freq / sampleRate) * 0.3 *
+                    data[i] += Math.sin(writePos * Math.PI * 2 * currNote[1] / sampleRate) * 0.5 *
                         (
-                            (noteDur[0] < 2000 ? noteDur[0] / 2000 : 1) *
-                            (noteDur[1] < 8000 ? noteDur[1] / 8000 : 1)
+                            (noteDur[0] < taper ? noteDur[0] / taper : 1) *
+                            (noteDur[1] < taper ? noteDur[1] / taper : 1)
                         )
-                        
                 }
-                data[i] = value
             }
             audio.mozWriteAudio(data)
         }
@@ -96,4 +117,5 @@ function play(){
     }
 
     writeBits()
-}
+
+}()
